df6920e15d14714bdd4312f02079f0255decffa6
EngredV <vanegas.engred@gmail.com>
2021-01-08 06:17:24 -0500

    Added all files required for assignment-5

--- script.js
+++ script.js
@@ -1,38 +1,117 @@
-const tbody = document.getElementsByTagName("tbody");
-const addColumn = document.querySelector("#add-column");
-const addRows = document.querySelector("#add-row");
-const removeColumn = document.querySelector("#remove-column");
-const removeRow = document.querySelector("#remove-row");
-addColumn.addEventListener("click", function () {
-    const allColumns = document.querySelectorAll("tr");
-    for (let i = 0; i < allColumns.length; i++) {
-        let newCol = document.createElement("td");
-        newCol.className = "cube";
-        allColumns[i].appendChild(newCol);
-    }
-})
+let columns = 1;
+let selectedColor = "";
+let mouseOver = false;
+let currentColor = "";
 
-addRows.addEventListener("click", function () {
-    const countCol = document.querySelector("tr"); // Count for how many coilumns
-    const newRow = document.createElement("tr");
-    newRow.className = "cube";
-    tbody[0].appendChild(newRow);
-    for (let i = 0; i < countCol.children.length; i++) {
-        let newCell = document.createElement("td");
-        newCell.className = "cube";
-        newRow.append(newCell);
-    }
-})
+// Fills all cells with selected color
+function selectColor(color) {
+    selectedColor = color;
+}
 
-removeColumn.addEventListener("click", function () {
-    const allColumns = document.querySelectorAll("tr");
-    console.log(allColumns);
-    for (let i = 0; i < allColumns.length; i++) {
-        allColumns[i].lastElementChild.remove();
+// Adds columns to table
+function addColumn() {
+    let row = document.getElementsByClassName("row");
+    let tr = Array.from(row);
+    for (let i = 0; i < tr.length; i++) {
+        let cell = document.createElement("td");
+        cell.classList.add("cell");
+        colorChange(cell);
+        tr[i].appendChild(cell);
     }
-})
+    columns++;
+}
 
-removeRow.addEventListener("click", function () {
-    const firstRow = document.querySelector("tr");
-    firstRow.remove();
-})
+// Removes columns to table
+function removeColumn() {
+    let row = document.getElementsByClassName("row");
+    let tr = Array.from(row);
+    if (tr.length == 0) {
+        alert('Error: there is nothing to remove');
+    return this;
+    }
+    for (let i = 0; i < tr.length; i++) {
+        tr[i].removeChild(tr[i].lastChild);
+    }
+    columns--;
+}
+
+// Adds rows to table
+function addRow() {
+    let table = document.getElementById("table");
+    let row = document.createElement("tr");
+    row.classList.add("row");
+    table.appendChild(row);
+    for(let i = 0; i < columns; i++) {
+        let cell = document.createElement("td");
+        cell.classList.add("cell");
+        colorChange(cell);
+        row.appendChild(cell);
+    }
+}
+
+// Removes rows from table
+function removeRow() {
+    let row = document.getElementsByClassName("row");
+    let tr = Array.from(row);
+    if (tr.length == 0) {
+        alert('Error: there is nothing to remove');
+    return this;
+    }
+    tr[tr.length - 1].parentNode.removeChild(tr[tr.length - 1]);
+}
+
+// Fills in all uncolored cells with selected color
+function fillAllUncoloredCells() {
+    let allCells = document.getElementsByTagName("td");
+    for (let i = 0; i < allCells.length; i++) {
+        if (allCells[i].style.backgroundColor == "") {
+            allCells[i].style.backgroundColor = selectedColor;
+        }
+    }
+}
+
+// Fills all cells with selected color
+function fillAllCells() {
+    let allCells = document.getElementsByTagName("td");
+    for (let i = 0; i < allCells.length; i++) {
+        allCells[i].style.backgroundColor = selectedColor;
+    }
+}
+
+// Clears all cells of color
+function clearAllCells() {
+    let allCells = document.getElementsByTagName("td");
+    for (let i = 0; i < allCells.length; i++) {
+        allCells[i].style.backgroundColor = "";
+    }
+}
+
+// Allows colors to be changed based on dropdown selection
+function colorChange(cell){
+    cell.addEventListener("click", setColor);
+
+    cell.addEventListener("mousedown", function() {
+        mouseOver = true;
+        currentColor = selectedColor;
+    })
+
+    cell.addEventListener("mousemove" , function() {
+        if (mousOver == true)
+        {
+            cell.style.backgroundColor = currentColor;
+        }
+    })
+
+    cell.addEventListener("mouseup" , function() {
+        if (mouseOver == true)
+        {
+            mouseOver = false;
+        }
+    })
+}
+
+// Sets color based on dropdown selection
+function setColor(){
+    this.style.backgroundColor = selectedColor;
+}
+
